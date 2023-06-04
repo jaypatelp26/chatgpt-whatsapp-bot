0e1abb2bea3be584027124f03e7189cebf55efa1
Jay Girishkumar Patel <84357501+JayPatel77470@users.noreply.github.com>
2023-06-03 21:23:40 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 const axios = require('axios');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const bodyParser = require('body-parser');
+const api_key = process.env.API_KEY;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -33,7 +34,7 @@ app.listen(3000, () => {
 async function generateChatResponse(message) {
     const headers = {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer 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',
+        'Authorization': 'Bearer ' + api_key,
         'User-Agent': 'curl/7.64.1',
     };
     const body = {
